810e49c8c35be33c51c31054d672b15755561bfd
Sebastian Isufaj <seb.encod@gmail.com>
2022-11-28 12:04:24 +0100

    Create recipe.js

---
+++ recipe.js
@@ -0,0 +1,18 @@
+console.log("Welcome to programing kithchen.");
+let sugar = 80;
+let butter = 120;
+let flour = 200;
+
+console.log("Sugar:", sugar);
+console.log("Butter:", butter);
+console.log("Flour:", flour);
+
+let mediumBowl;
+mediumBowl = butter + sugar;
+console.log("We mixed together butter with sugar:");
+console.log("Content of mediumBowl:", mediumBowl);
+
+mediumBowl += mediumBowl + flour;
+
+console.log("We also added flour...");
+console.log("Now the content is :", mediumBowl);
