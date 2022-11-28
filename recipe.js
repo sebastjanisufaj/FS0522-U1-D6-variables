34f0099994655999c4016de54fc426354d48216f
Sebastian Isufaj <seb.encod@gmail.com>
2022-11-28 13:33:48 +0100

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -12,7 +12,7 @@ mediumBowl = butter + sugar;
 console.log("We mixed together butter with sugar:");
 console.log("Content of mediumBowl:", mediumBowl);
 
-mediumBowl += mediumBowl + flour;
+mediumBowl += flour + sugar;
 
 console.log("We also added flour...");
 console.log("Now the content is :", mediumBowl);
